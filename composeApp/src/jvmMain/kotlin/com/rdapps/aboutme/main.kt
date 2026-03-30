0c434c3ed665563be239326794904f264ffff580
Rajdeep Vaghela <rajdeep.vaghela610@gmail.com>
2026-03-30 17:42:48 +0530

    Set application icon for JVM desktop window
    - Add `ic_launcher.webp` to common resources for use across platforms.
    - Update the `Window` configuration in `jvmMain` to display the application icon using `painterResource`.

--- composeApp/src/jvmMain/kotlin/com/rdapps/aboutme/main.kt
+++ composeApp/src/jvmMain/kotlin/com/rdapps/aboutme/main.kt
@@ -1,16 +1,20 @@
 package com.rdapps.aboutme
 
+import aboutme.composeapp.generated.resources.Res
+import aboutme.composeapp.generated.resources.ic_launcher
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import org.jetbrains.compose.resources.painterResource
 
 fun main() = application {
     val state = rememberWindowState(width = 1200.dp, height = 800.dp)
     Window(
         onCloseRequest = ::exitApplication,
         title = "About Me",
-        state = state
+        state = state,
+        icon = painterResource(Res.drawable.ic_launcher)
     ) {
         App()
     }
